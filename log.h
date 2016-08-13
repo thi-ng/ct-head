2d38801b8c1be3cfd32db485f2d5bc95b8a68292
Karsten Schmidt <k@postspectacular.com>
2016-08-13 22:45:07 +0100

    add .clang-format, update formatting

--- log.h
+++ log.h
@@ -19,23 +19,23 @@
 #ifdef NDEBUG
 #define CT_DEBUG(M, ...)
 #else
-#define CT_DEBUG(M, ...)                                                       \
+#define CT_DEBUG(M, ...) \
   CT_LOG_PRELUDE "[  DEBUG] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
-#endif // NDEBUG
+#endif  // NDEBUG
 
-#define CT_ERROR(M, ...)                                                       \
-  CT_LOG_PRELUDE                                                               \
-	  CT_ANSI_RED "[  ERROR] (%s:%d) " M CT_ANSI_RESET "\n", \
+#define CT_ERROR(M, ...)                                         \
+  CT_LOG_PRELUDE                                                 \
+          CT_ANSI_RED "[  ERROR] (%s:%d) " M CT_ANSI_RESET "\n", \
 	  __FILE__, __LINE__, ##__VA_ARGS__)
 
-#define CT_WARN(M, ...)                                                        \
+#define CT_WARN(M, ...)                                           \
   CT_LOG_PRELUDE "[   WARN] (%s:%d) " M "\n", __FILE__, __LINE__, \
 	  ##__VA_ARGS__)
 
-#define CT_INFO(M, ...)                                                        \
+#define CT_INFO(M, ...) \
   CT_LOG_PRELUDE "[   INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 
-#define CT_SUCCESS(M, ...)                                                     \
+#define CT_SUCCESS(M, ...)                                                \
   CT_LOG_PRELUDE CT_ANSI_GREEN "[SUCCESS] (%s:%d) " M CT_ANSI_RESET "\n", \
 	  __FILE__, __LINE__, ##__VA_ARGS__)
 
@@ -47,7 +47,7 @@
 #define CT_INFO(M, ...)
 #define CT_SUCCESS(M, ...)
 
-#endif // CT_FEATURE_LOG
+#endif  // CT_FEATURE_LOG
 
 ////////////////////////////////////////////////////////////
 
@@ -57,16 +57,16 @@
 
 #ifdef CT_FEATURE_CHECKS
 
-#define CT_CHECK(A, M, ...)                                                    \
-  if (!(A)) {                                                                  \
-    CT_ERROR(M, ##__VA_ARGS__);                                                \
-    CT_CHECK_FAIL_ACTION;                                                      \
+#define CT_CHECK(A, M, ...)     \
+  if (!(A)) {                   \
+    CT_ERROR(M, ##__VA_ARGS__); \
+    CT_CHECK_FAIL_ACTION;       \
   }
 
-#define CT_CHECK_DEBUG(A, M, ...)                                              \
-  if (!(A)) {                                                                  \
-    CT_DEBUG(M, ##__VA_ARGS__);                                                \
-    CT_CHECK_FAIL_ACTION;                                                      \
+#define CT_CHECK_DEBUG(A, M, ...) \
+  if (!(A)) {                     \
+    CT_DEBUG(M, ##__VA_ARGS__);   \
+    CT_CHECK_FAIL_ACTION;         \
   }
 
 #else
@@ -74,12 +74,12 @@
 #define CT_CHECK(A, M, ...)
 #define CT_CHECK_DEBUG(A, M, ...)
 
-#endif // CT_FEATURE_CHECKS
+#endif  // CT_FEATURE_CHECKS
 
-#define CT_SENTINEL(M, ...)                                                    \
-  {                                                                            \
-    CT_ERROR(M, ##__VA_ARGS__);                                                \
-    CT_CHECK_FAIL_ACTION;                                                      \
+#define CT_SENTINEL(M, ...)     \
+  {                             \
+    CT_ERROR(M, ##__VA_ARGS__); \
+    CT_CHECK_FAIL_ACTION;       \
   }
 
 ////////////////////////////////////////////////////////////
@@ -90,10 +90,10 @@
 #define CT_CHECK_MEM_FAIL_ACTION exit(1)
 #endif
 
-#define CT_CHECK_MEM(A)                                                        \
-  if (!(A)) {                                                                  \
-    CT_ERROR("Out of memory!");                                                \
-    CT_CHECK_MEM_FAIL_ACTION;                                                  \
+#define CT_CHECK_MEM(A)         \
+  if (!(A)) {                   \
+    CT_ERROR("Out of memory!"); \
+    CT_CHECK_MEM_FAIL_ACTION;   \
   }
 
 #else
@@ -106,11 +106,11 @@
 
 #ifdef CT_FEATURE_LOG
 
-#define CT_TIMED(...)                                                          \
-  {                                                                            \
-    clock_t t0 = clock();                                                      \
-    __VA_ARGS__;                                                               \
-    CT_INFO("time: %1.3fms", (double)(clock() - t0) / CLOCKS_PER_SEC * 1e3);   \
+#define CT_TIMED(...)                                                        \
+  {                                                                          \
+    clock_t t0 = clock();                                                    \
+    __VA_ARGS__;                                                             \
+    CT_INFO("time: %1.3fms", (double)(clock() - t0) / CLOCKS_PER_SEC * 1e3); \
   }
 #else
 
